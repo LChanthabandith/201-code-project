c8d70c056830fa85f1dce10030ab4f56254c5559
Lavieng Chanthabandith <LChanthabandith@gmail.com>
2023-04-19 04:58:35 -0500

    adding questions 6 and 7 to JS

--- app.js
+++ app.js
@@ -7,42 +7,56 @@ alert(
 );
 console.log(`User's name is ${name}`);
 
+// Array of possible answers for question 7
+let possibleAnswers = ["Paris", "New York", "Tokyo", "London", "Sydney", "Dubai"];
+// Keep track of correct answers
+let correctAnswers = 0;
+
+// Question 1
 let answer1 = prompt("Do I live in Tennessee?").toLowerCase();
 if (answer1 === "yes" || answer1 === "y") {
   alert("You are right! I live in Murfreesboro, Tennessee");
+  correctAnswers++;
 } else {
   alert("Sorry, that is incorrect. I do live in Tennessee.");
 }
 
-let answer2 = prompt("Have I attended college?").toUpperCase();
-if (answer2 === "YES" || answer2 === "Y") {
+// Question 2
+let answer2 = prompt("Have I attended college?").toLowerCase();
+if (answer2 === "yes" || answer2 === "y") {
   alert("Correct! I attended Motlow State Community College.");
+  correctAnswers++;
 } else {
   alert("Incorrect. I have attended college.");
 }
 
+// Question 3
 let answer3 = prompt("Have I worked in the restaurant industry?").toLowerCase();
-if (answer3 === "no" || answer3 === "n") {
-  alert(
-    "I am sorry, that is incorrect. I own a family restaurant in Murfreesboro, Tennessee."
-  );
-} else {
+if (answer3 === "yes" || answer3 === "y") {
   alert(
     "Yes, you are correct! I own a family restaurant located in Murfreesboro, Tennessee."
   );
+  correctAnswers++;
+} else {
+  alert(
+    "I am sorry, that is incorrect. I own a family restaurant in Murfreesboro, Tennessee."
+  );
 }
 
+// Question 4
 let answer4 = prompt(
   "Do I plan on continuing in the restaurant industry?"
-).toUpperCase();
+).toLowerCase();
 if (answer4 === "no" || answer4 === "n") {
   alert("Correct! I plan on expanding my future career into coding.");
+  correctAnswers++;
 } else {
   alert(
     "That is incorrect. I actually plan on expanding my future career into coding."
   );
 }
 
+// Question 5
 let answer5 = prompt(
   "Am I taking courses at Code X Academy to become a Full-Stack Engineer?"
 ).toLowerCase();
@@ -50,25 +64,27 @@ if (answer5 === "yes" || answer5 === "y") {
   alert(
     "Yes! I am currently enrolled with Code X Academy to become a Full-Stack Engineer."
   );
+  correctAnswers++;
 } else {
   alert(
     "That is incorrect. I am currently enrolled with Code X Academy to become a Full-Stack Engineer."
   );
 }
 
+// Console Logs
 if (answer1 === "yes" || answer1 === "y") {
   console.log("User got question 1 correct");
 } else {
   console.log("User got question 1 wrong");
 }
 
-if (answer2 === "yes" || answer1 === "y") {
+if (answer2 === "yes" || answer2 === "y") {
   console.log("User got question 2 correct");
 } else {
   console.log("User got question 2 wrong");
 }
 
-if (answer3 === "yes" || answer1 === "y") {
+if (answer3 === "yes" || answer3 === "y") {
   console.log("User got question 3 correct");
 } else {
   console.log("User got question 3 wrong");
@@ -80,12 +96,89 @@ if (answer4 === "no" || answer4 === "n") {
   console.log("User got question 4 wrong");
 }
 
-if (answer5 === "yes" || answer1 === "y") {
+if (answer5 === "yes" || answer5 === "y") {
   console.log("User got question 5 correct");
 } else {
   console.log("User got question 5 wrong");
 }
 
 alert(
-  `Thanks for getting to know me, ${name}! Read my page for detailed information about me.`
+  `Thanks for getting to know me, ${name}! Let's play a random guessing game before you read my page for detailed information about me.`
 );
+
+// Question 6
+let answer6 = [5, 10, 15, 20];
+let attempts6 = 6;
+let userAnswer6 = parseInt(prompt("Guess a number between 1 and 25 that's divisible by 5:"));
+
+while (attempts6 > 0) {
+  console.log("User's guess: " + answer6);
+  let isCorrect = false;
+  for (let i = 0; i < answer6.length; i++) {
+    if (userAnswer6 === answer6[i]) {
+    alert("Correct! The possible answers were: " + answer6[0] + ", " + answer6[1] + ", " + answer6[2] + ", " + answer6[3]);
+    correctAnswers++;
+    isCorrect = true;
+    break;
+    }
+  }
+  if (isCorrect) {
+    break;
+  } else {
+    attempts6--;
+    if (userAnswer6 > 20) {
+      alert("Too high!");
+    } else if (userAnswer6 < 5) {
+      alert("Too low!");
+    } else if (userAnswer6 % 5 !== 0) {
+      alert("That's not divisible by 5!");
+    } else {
+      alert("Sorry, that's not one of the possible answers.");
+    }
+    if (attempts6 > 0) {
+      userAnswer6 = parseInt(prompt("Guess again!"));
+    }
+  }
+}
+
+if (attempts6 === 0) {
+  console.log("User is out of attempts.");
+  alert("Sorry, you ran out of attempts. The possible answers were: " + answer6[0] + ", " + answer6[1] + ", " + answer6[2] + ", " + answer6[3]);
+} else {
+    console.log("User guessed correctly.");
+}
+
+// Question 7
+let answer7;
+let answerCount = 6;
+let answered = false;
+
+while (answerCount > 0) {
+  answer7 = prompt("Name a city with a famous landmark:");
+  for (var i = 0; i < possibleAnswers.length; i++) {
+    if (answer7.toLowerCase() === possibleAnswers[i].toLowerCase()) {
+      alert("Correct!");
+      correctAnswers++;
+      answered = true;
+      break;
+    }
+  }
+  if (answered) {
+    break;
+  }
+  alert("Incorrect. Guesses left: " + (answerCount - 1));
+  answerCount--;
+}
+if (answerCount == 0) {
+  alert("Out of guesses. Possible answers were: " + possibleAnswers.join(", "));
+}
+
+{
+  console.log("Question 7 - Name a city with a famous landmark:");
+  console.log("Possible correct answers: " + possibleAnswers.join(", "));
+  console.log("User's answer: " + answer7);
+  console.log("User got it correct? " + answer7);
+}
+
+// Display total correct answers
+alert("You got " + correctAnswers + " out of 7 correct.");
